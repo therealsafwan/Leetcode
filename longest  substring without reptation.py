180b5caf8db8d2bd23b8b5c4880820a498f8ae33
Safwan S M <chappu230@gmail.com>
2025-05-05 19:21:33 +0530

    commit

---
+++ longest  substring without reptation.py
@@ -0,0 +1,19 @@
+#Given a string s, find the length of the longest substring without duplicate characters.
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_set = set()
+        left = 0
+        max_len = 0
+
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
---
+++ two sum.py
@@ -0,0 +1,14 @@
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.You may assume that each input would have exactly one solution, and you may not use the same element twice.You can return the answer in any order.
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
